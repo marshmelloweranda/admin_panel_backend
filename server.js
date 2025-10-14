dc7f27d04e69c008a1e79a83d4235f56402604cf
marshmelloweranda <dasunw@pmu.mode.gov.lk>
2025-10-14 10:05:09 +0530

    import changed

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
-const applicationRoutes = require('./Routes/Application');
+const applicationRoutes = require('./Routes/Applications');
 require('dotenv').config();
 
 const app = express();
