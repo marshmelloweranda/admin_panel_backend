6421a819183953725c40c5f44e4a7fe4bd11d1ed
UwanRukshala <34532108+UwanRukshala@users.noreply.github.com>
2025-10-14 09:36:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const router = express.Router();
 const cors = require('cors');
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
 const applicationRoutes=require('/Routes/Application');
 require('dotenv').config();
 
+
+
 const app = express();
 const PORT = process.env.PORT || 5000;
+app.use('/aapi', router);
+
 
 // Rate limiting
 const limiter = rateLimit({
